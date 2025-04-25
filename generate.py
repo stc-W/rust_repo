8f1aa15df398ce1f348ae5ca5305287045e31ebe
edalring <398807347@qq.com>
2025-04-25 19:56:03 +0800

    generate under 'dataset' and collect failed cases

--- generate.py
+++ generate.py
@@ -78,7 +78,7 @@ def get_crates_name(lib):
     except Exception as e:
         return None
 download_repo()
-install_all_require_rust_toolchain()
+# install_all_require_rust_toolchain()
 for index, row in complete.iterrows():
     if not pd.isna(row["edition"]):
         complete.at[index, "edition"] = str(complete.at[index, "edition"]).split(".")[0]
@@ -93,6 +93,11 @@ for index, row in complete.iterrows():
         complete.at[index, "std_version"] = edition[row["edition"]]
 
 for index, row in complete.iterrows():
+    # Save the rust projects to the "dataset" directory
+    if not os.path.exists("dataset"):
+        os.makedirs("dataset")
+    dataset_path = "dataset"
+    failed_list = []
     try:
         cve_id = row["cve_id"]
         edition = row["edition"]
@@ -100,62 +105,79 @@ for index, row in complete.iterrows():
         repo_url = row["repo_url"]
         repair_hash = row["commits_sha"]
         name = get_crates_name(lib)
+        
+        if os.path.exists(f"{dataset_path}/{cve_id}_repair"):
+            print(f"Project {cve_id}已经存在, 跳过")
+            continue
+        
         if name is None:
             print(f"❌ cve_id: {cve_id}, lib: {lib}有问题, 查询不到包名")
             continue
+        
+        
         if pd.isna(row["commits_sha_begin"]):
             bug_hash = get_earliest_parent(repair_hash, lib)
         else:     
             bug_hash = get_earliest_parent(row["commits_sha_begin"], lib)
-        result1 = subprocess.run(["cargo", "new", f"{cve_id}_repair", "--edition", str(edition)], capture_output=True, text=True)
+        result1 = subprocess.run(["cargo", "new", f"{cve_id}_repair", "--edition", str(edition)], capture_output=True, text=True, cwd=dataset_path)
         if result1.returncode == 0:
             print(f"✅ Project {cve_id}_repair created successfully.")
         else:
             print(f"❌ Failed to create Rust project {cve_id}.")
             print("Error message:", result1.stderr)
             continue
-        result2 = subprocess.run(["cargo", "new", f"{cve_id}_bug", "--edition", str(edition)], capture_output=True, text=True)
+        result2 = subprocess.run(["cargo", "new", f"{cve_id}_bug", "--edition", str(edition)], capture_output=True, text=True, cwd=dataset_path)
+        
         if result2.returncode == 0:
             print(f"✅ Project {cve_id}_bug created successfully.")
         else:
             print(f"❌ Failed to create Rust project {cve_id}.")
             print("Error message:", result2.stderr)
-        with open(f"./{cve_id}_repair/rust-toolchain.toml", "w") as f1:
+            
+        with open(f"./{dataset_path}/{cve_id}_repair/rust-toolchain.toml", "w") as f1:
             f1.write(f'''[toolchain]\nchannel = "{row["std_version"]}"''')
             f1.flush()
             f1.close()
-        with open(f"./{cve_id}_bug/rust-toolchain.toml", "w") as f2:
+        with open(f"./{dataset_path}/{cve_id}_bug/rust-toolchain.toml", "w") as f2:
             f2.write(f'''[toolchain]\nchannel = "{row["std_version"]}"''')
             f2.flush()
             f2.close()
-        with open(f"./{cve_id}_repair/Cargo.toml", "a") as f3:
+        with open(f"./{dataset_path}/{cve_id}_repair/Cargo.toml", "a") as f3:
             f3.write(f'''{name} = {{git = "{repo_url}", rev = "{repair_hash}"}}''')
             f3.flush()
             f3.close()
-        with open(f"./{cve_id}_bug/Cargo.toml", "a") as f4:
+        with open(f"./{dataset_path}/{cve_id}_bug/Cargo.toml", "a") as f4:
             f4.write(f'''{name} = {{git = "{repo_url}", rev = "{bug_hash}"}}''')
             f4.flush()
             f4.close()
+            
         rust_code = get_response(issue_url=row["issues"], lib=lib, crate=name, commit_hash=repair_hash)
         if rust_code is not None:
-            with open(f"./{cve_id}_repair/src/main.rs", "w") as f5:
+            with open(f"./{dataset_path}/{cve_id}_repair/src/main.rs", "w") as f5:
                 f5.write(rust_code)
                 f5.flush()
                 f5.close()
-            with open(f"./{cve_id}_bug/src/main.rs", "w") as f6:
+            with open(f"./{dataset_path}/{cve_id}_bug/src/main.rs", "w") as f6:
                 f6.write(rust_code)
                 f6.flush()
                 f6.close()    
         print(f"✅ Project {cve_id} 填充完成.")
     except Exception as e:
-        path1 = f"./{cve_id}_repair/"
-        path2 = f"./{cve_id}_bug/"        
+        path1 = f"./{dataset_path}/{cve_id}_repair/"
+        path2 = f"./{dataset_path}/{cve_id}_bug/"        
         if os.path.exists(path1):
             shutil.rmtree(path1)
         if os.path.exists(path2):
             shutil.rmtree(path2)
         print(f"❌ Project {cve_id} 填充失败.")
         print(f"Err message: {e}")
+        failed_list.append(cve_id)
+        
+    with open("failed_list.txt", "w") as f:
+        for item in failed_list:
+            f.write(f"{item}\n")
+            f.flush()
+            f.close()
 
    
         
